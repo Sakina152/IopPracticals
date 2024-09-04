d31aabeb27cd351b9cb0f55d515abdc4419dbc1c
Sakina152 <skdhilawala1280@gmail.com>
2024-09-04 11:53:19 +0530

    Add files via upload

---
+++ P31.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main(){
+    int n,sum=0,digit;
+    printf("Enter a number:\n ");
+    scanf("%d",&n);
+     //processing each digit of a number
+     while(n>0){
+        digit=n%10;
+        sum=sum+digit;
+        n=n/10;
+     }
+
+     printf("Sum of the digits of number = %d",sum);
+     return 0;
+
+
+}
