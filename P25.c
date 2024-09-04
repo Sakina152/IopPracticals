872c826ccab8d016e4667c4f1b50645ca5fc4ab3
Sakina152 <skdhilawala1280@gmail.com>
2024-09-04 11:01:57 +0530

    Add files via upload

---
+++ P25.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(){
+    int a;
+    printf("Enter a number:\n");
+    scanf("%d",&a);
+    for(int i=1;i<=10;i++){
+        printf("%d * %d = %d\n",a,i,a*i);
+
+    }
+
+}
