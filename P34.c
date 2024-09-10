e97b2ee03155c6e191abf420de1442a68a8e2492
Sakina152 <skdhilawala1280@gmail.com>
2024-09-10 23:18:09 +0530

    Add files via upload

---
+++ P34.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main() {
+    int n; 
+    printf("Enter the number of Fibonacci numbers to print: ");
+    scanf("%d", &n); 
+
+    if (n <= 0) {
+        printf("Please enter a positive integer.\n");
+        return 1; 
+    }
+
+    int a = 0;
+    int b = 1;
+
+    printf("Fibonacci sequence:\n");
+
+    for (int i = 1; i <= n; i++) {
+        printf("%d ", a); // Print the current Fibonacci number
+        int c = a + b; // Calculate the next Fibonacci number
+        a = b; // Update a to the next Fibonacci number
+        b = c; // Update b to the next Fibonacci number
+    }
+
+    
+    return 0; 
+}
