6718f6e33fa19b6f08970cd814d694bd26a1a606
Sakina152 <skdhilawala1280@gmail.com>
2024-09-10 22:32:02 +0530

    Add files via upload

---
+++ P30.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main(){
+    int n,revn=0,r;//r=remainder revn=reversed number
+    printf("Enter an integer:\n");
+    scanf("%d",&n);
+
+    while(n!=0){
+        r=n%10;
+        revn=revn*10+r;
+        n/=10;
+    }
+    printf("Reversed number: %d",revn);
+
+}
