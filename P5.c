2b5f25ece0f6b58ef0c443b6711ef8640fe63abe
Sakina152 <skdhilawala1280@gmail.com>
2024-09-11 11:45:02 +0530

    P5.c

--- P5.c
+++ P5.c
@@ -2,7 +2,7 @@
 int main(){
     int a,b,c;
     printf("Enter two numbers:\n");
-    scanf("%d %d",a,b);
+    scanf("%d %d",&a,&b);
     printf("Before swapping a=%d ,b=%d\n",a,b);
     c=a;
     a=b;
