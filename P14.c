292ac65e95a04add9825c35d4fee7dc1a5f207b7
Sakina152 <skdhilawala1280@gmail.com>
2024-09-12 12:23:41 +0530

    P14.c

--- P14.c
+++ P14.c
@@ -2,7 +2,7 @@
 int main(){
     char ch;
     printf("Enter character :\n");
-    scanf("%c",&ch);
+    scanf("%c",&ch);//or use ch=getchar()
     if(ch>='A' && ch<='Z'){
         printf("Uppercase");
 
