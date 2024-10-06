9ef76a818aac9f557871a3ed7e3a3442640bf607
Sakina152 <skdhilawala1280@gmail.com>
2024-10-06 18:16:14 +0530

    P68.c

--- P68.c
+++ P68.c
@@ -1,9 +1,6 @@
 #include <stdio.h>
  int main(){
- int arr1[3][3];
- int arr2[3][3];
- int arr3[3][3];
- 
+ int arr1[3][3],arr2[3][3],arr3[3][3],sum=0;
  printf("Enter integers for 3*3 matrix 1:\n");
     for(int i=0;i<3;i++){
         for(int j=0;j<3;j++){
@@ -18,14 +15,16 @@
             scanf("%d",&arr2[i][j]);
         }
     }
-
     printf("Multiplication of matrixes is :\n");
     for(int i=0;i<3;i++){
         for(int j=0;j<3;j++){
-            arr3[i][j]=arr1[i][j]*arr2[i][j];
+            for(int k=0;k<3;k++){
+            sum+=arr1[i][k]*arr2[k][j];
+            }
+            arr3[i][j]=sum;
             printf("%d \t",arr3[i][j]);
+            sum=0;
         }
         printf("\n");
     }
- 
 }
